d7b969a34d871324060a3fbb03f9dd3749ee70e1
dvogt23 <divogt@live.de>
2017-07-11 11:44:18 +0200

    i3 gitconfig

--- make.sh
+++ make.sh
@@ -1,1 +1,23 @@
-#/bin/bashdir=~/.dotfilesolddir=~/.dotfiles.backupfiles="bash_profile gitattirbutes gitconfig gitignore hushlogin"########### create dotfiles_old in homedirecho "Creating $olddir for backup of any existing dotfiles in ~"mkdir -p $olddir# change to the dotfiles directoryecho "Changing to the $dir directory"cd $dir# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks for file in $files; do    echo "Moving any existing dotfiles from ~ to $olddir"    mv ~/.$file ~/$olddir/    echo "Creating symlink to $file in home directory."    ln -s $dir/$file ~/.$filedonesource osx.shsource brew.sh
+#/bin/bash
+
+dir=~/.dotfiles
+olddir=~/.dotfiles.backup
+files="bash_profile gitattirbutes gitconfig gitignore gitattributes hushlogin vim vimrc"
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving $file to $olddir"
+    mv ~/.$file ~/$olddir/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
