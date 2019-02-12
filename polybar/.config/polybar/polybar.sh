bbb1428f04893fb120ea7639f859369627039b43
dvogt23 <divogt@live.de>
2019-02-12 15:33:10 +0100

    polybar local configs

--- polybar/.config/polybar/polybar.sh
+++ polybar/.config/polybar/polybar.sh
@@ -6,10 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+ETH=$(ip link show | grep "state UP" | awk '{print $2}' | sed 's/://')
 # Launch bar1 and bar2
-polybar top &
-if [ -n "$MONITOR2" ]; then
-    polybar top2 &
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+       IFETH=$ETH MONITOR=$m polybar top &
+    done
+else
+    polybar --reload top &
 fi
 
 echo "Bars launched..."
